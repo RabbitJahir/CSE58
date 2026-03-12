66dc0df582f42ab6573bc972ab362be1997ef9c0
RabbitJahir <alltimer785@gmail.com>
2026-03-12 09:15:47 +0600

    added oop mid notes

--- Client/src/S12/Oop/OopMid.jsx
+++ Client/src/S12/Oop/OopMid.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
 
 const OopMid = () => {
   return (
-    <div>OopMid</div>
-  )
-}
+    <div className="mid-container">
+      <a href="/S12/Oop/Variables_and_Types_Summary.pdf" className="SubButton ">
+        Variables_&_Types_Summary<sub> Download</sub>
+      </a>
 
-export default OopMid
+      <a href="/S12/Oop/Java_Control_Statements.pdf" className="SubButton ">
+        Java_Control_Statements<sub> Download</sub>
+      </a>
+
+      <a href="/S12/Oop/Java_Constructor.pdf" className="SubButton ">
+        Java_Constructor<sub> Download</sub>
+      </a>
+
+      <a href="/S12/Oop/Java_Access_Modifiers.pdf" className="SubButton ">
+        Java_Access_Modifiers<sub> Download</sub>
+      </a>
+    </div>
+  );
+};
+
+export default OopMid;
