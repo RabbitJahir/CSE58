40b179307c3f98573e14799c3407dbcdea6551ed
RabbitJahir <alltimer785@gmail.com>
2026-03-16 01:49:28 +0600

    java added

--- Client/src/S12/Oop/OopMid.jsx
+++ Client/src/S12/Oop/OopMid.jsx
@@ -3,25 +3,27 @@ import React from "react";
 const OopMid = () => {
   return (
     <div className="mid-container">
-      <a
-        href="/S12/Oop/Variables_and_Types_Summary.pdf"
-        className="SubButton "
-      >
-        Variables_&_Types_Summary<sub> Download</sub>
+      <a href="/S12/Oop/java_basics.pdf" className="SubButton ">
+        Java Basics<sub> Download</sub>
       </a>
 
       <a
-        href="/S12/Oop/Java_Control_Statements.pdf"
+        href="/S12/Oop/Java_Variables_and_Types_Summary.pdf"
         className="SubButton "
       >
-        Java_Control_Statements<sub> Download</sub>
+        Java Variables & Types Summary<sub> Download</sub>
       </a>
 
-      <a
-        href="/S12/Oop/Java_Constructor.pdf"
-        className="SubButton "
-      >
-        Java_Constructor<sub> Download</sub>
+      <a href="/S12/Oop/Java_Control_Statements.pdf" className="SubButton ">
+        Java Control Statements<sub> Download</sub>
+      </a>
+
+      <a href="/S12/Oop/Java_Classes_and_Objects.pdf" className="SubButton ">
+        Java Classes and Objects<sub> Download</sub>
+      </a>
+
+      <a href="/S12/Oop/Java_Constructor.pdf" className="SubButton ">
+        Java Constructor<sub> Download</sub>
       </a>
     </div>
   );
