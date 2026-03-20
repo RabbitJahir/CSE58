a164f8d7ce6a269517253a2ba40db7d5ea642c5c
RabbitJahir <alltimer785@gmail.com>
2026-03-21 00:58:57 +0600

    fixing vercel deplyement

---
+++ Client/.next/dev/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"C1m+/ExmdFSq99ewugITzKKWtE0tZEVqk8XmG2j7HvA=\"\n}"
--- Client/app/page.tsx
+++ Client/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div>
       <header className="homeheader flex items-center justify-center gap-4 p-4 bg-white dark:bg-black/30 backdrop-blur-sm relative">
-        <div className="left-4 top-1/2 -translate-y-1/2">
+        <div className="absolute left-4 top-1/2 -translate-y-1/2">
           <Image src="/favicon.ico" alt="logo" width={32} height={32} />
         </div>
 
@@ -23,6 +23,44 @@ export default function Home() {
       </header>
 
       <main className="main-box flex flex-col items-center gap-4 mt-6">
+        {/* 3 main links */}
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 w-full max-w-6xl">
+          <Link
+            href="https://ouits-res.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="homebox"
+          >
+            <h2>OU1TS</h2>
+            <p>
+              Create an account and start exploring! <br />
+               The beginning of an amazing journey! Discover handy resources and tools! For everyone!<br/>
+              <i>TIP</i> : go to tools, b1t scheduler.
+            </p>
+          </Link>
+
+          <Link
+            href="https://university-companion.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="homebox"
+          >
+            <h2>University Companion</h2>
+            <p>The company you wish you had before any exam. Mainly for CSE.</p>
+            <p>Find Questions of previous batches : ct, mid, final. </p>
+          </Link>
+
+          <Link
+            href="https://cse58.vercel.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="homebox"
+          >
+            <h2>CSE 58</h2>
+            <p>Journal of the academic journey of CSE Batch 58.</p>
+          </Link>
+        </div>
+{/*-------------------------------semesters--------------------------------------------------------- */}
         <Link className="SubButton" href="/S1">
           Semester 1
         </Link>
