6cf5e2fec75f6449e134d491168ab60da3c135f5
RabbitJahir <alltimer785@gmail.com>
2026-03-23 21:37:17 +0600

    fixing pwa offline and cache storage

--- Client/next.config.js
+++ Client/next.config.js
@@ -5,6 +5,37 @@ const withPWA = require("next-pwa")({
   clientsClaim: true,
   disable: process.env.NODE_ENV === "development",
 
+  additionalManifestEntries: [
+    { url: "/", revision: null },
+    { url: "/S1", revision: null },
+    { url: "/S2", revision: null },
+    { url: "/S2/discrete", revision: null },
+    { url: "/S2/discrete/DiscreteMid", revision: null },
+    { url: "/S2/discrete/DiscreteMid/DiscreteCh1", revision: null },
+    { url: "/S2/discrete/DiscreteMid/DiscreteCh2", revision: null },
+    { url: "/S2/discrete/DiscreteBook", revision: null },
+    { url: "/S2/discrete/DiscreteFinal", revision: null },
+    { url: "/S2/eee", revision: null },
+    { url: "/S2/eee/EeeMid", revision: null },
+    { url: "/S2/eee/EeeFinal", revision: null },
+    { url: "/S2/eee/EeeLab", revision: null },
+    { url: "/S2/oop", revision: null },
+    { url: "/S2/oop/OopMid", revision: null },
+    { url: "/S2/oop/OopFinal", revision: null },
+    { url: "/S2/oop/JavaCode", revision: null },
+    { url: "/S2/opde", revision: null },
+    { url: "/S2/opde/OpdeMid", revision: null },
+    { url: "/S2/opde/OpdeFInal", revision: null },
+    { url: "/S2/bs", revision: null },
+    { url: "/S2/bs/BsMid", revision: null },
+    { url: "/S2/bs/BsFinal", revision: null },
+    { url: "/S2/bs/BsHistory", revision: null },
+    { url: "/S2/wad", revision: null },
+    { url: "/S2/wad/WadCode", revision: null },
+    { url: "/S2/ed", revision: null },
+    { url: "/S2/calendar", revision: null },
+  ],
+
   runtimeCaching: [
     {
       // JS & CSS
@@ -44,7 +75,7 @@ const withPWA = require("next-pwa")({
       handler: "NetworkFirst",
       options: {
         cacheName: "page-cache-v1",
-        networkTimeoutSeconds: 5,
+        networkTimeoutSeconds: 3,
         expiration: {
           maxEntries: 50,
         },
