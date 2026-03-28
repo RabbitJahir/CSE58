b0dfe561e4589b9e6e2b7d66860e46a3ff525649
RabbitJahir <alltimer785@gmail.com>
2026-03-28 23:52:12 +0600

    testing with version control

--- Client/next.config.js
+++ Client/next.config.js
@@ -13,7 +13,7 @@ const withPWA = require("next-pwa")({
     { url: "/", revision: "v1.0.0" },
     { url: "/S1", revision: "v1.0.0" },
     { url: "/S2", revision: "v1.0.0" },
-    { url: "/S2/calendar", revision: "v1.0.0" },
+    { url: "/S2/calendar", revision: "v1.0.1" },
     { url: "/S2/Calendar/academic_calendar_2026.jpg", revision: "v1.0.1" },
     { url: "/S2/Calendar/routine2ndsem.png", revision: "v1.0.1" },
     { url: "/S2/discrete", revision: "v1.0.0" },
