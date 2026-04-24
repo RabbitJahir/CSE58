088f38a05c0632edf7262a36cf4ab4071b4f399c
RabbitJahir <alltimer785@gmail.com>
2026-04-24 07:41:10 +0600

    opde

--- Client/app/S2/opde/OpdeFinal/page.tsx
+++ Client/app/S2/opde/OpdeFinal/page.tsx
@@ -1,7 +1,19 @@
+import Link from "next/link";
+
 export default function Page() {
   return (
     <div>
-      <main className="mid-container"></main>
+      <main className="mid-container">
+         <Link
+          href="/S2/Opde/LDE-opde.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <button className="SubButton">
+            Non-Homogeneous LDE <sub>-Download, Thanks Forhad</sub>
+          </button>
+        </Link>
+      </main>
     </div>
   );
 }
