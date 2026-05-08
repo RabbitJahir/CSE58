d89981c087101f5847b1f08e10f9766440a7cda5
RabbitJahir <alltimer785@gmail.com>
2026-05-08 23:49:26 +0600

    java code

--- Client/app/S2/oop/OopLab/JavaCode/Codes.ts
+++ Client/app/S2/oop/OopLab/JavaCode/Codes.ts
@@ -205,4 +205,248 @@ public class modifier1 {
   },
 
   /* */
+  {
+    id: 6,
+    title: "Hybrid ",
+    description: "double classes",
+    code: `
+      interface Eat {
+    void eat();
+}
+
+interface Sleep {
+    void sleep();
+}
+
+interface Play {
+    void play();
+}
+
+abstract class Animal implements Eat, Sleep, Play {
+
+    String name;
+
+    Animal(String name) {
+        this.name = name;
+    }
+
+    void showName() {
+        System.out.println("Animal: " + name);
+    }
+}
+
+class Dog extends Animal {
+
+    Dog(String name) {
+        super(name);
+    }
+    
+    @Override
+    public void eat() {
+        System.out.println(name + " is eating");
+    }
+
+    @Override
+    public void sleep() {
+        System.out.println(name + " is sleeping");
+    }
+
+    @Override
+    public void play() {
+        System.out.println(name + " is playing");
+    }
+}
+
+class Parrot extends Animal {
+
+    Parrot(String name) {
+        super(name);
+    }
+
+    @Override
+    public void eat() {
+        System.out.println(name + " is eating");
+    }
+
+    @Override
+    public void sleep() {
+        System.out.println(name + " is sleeping");
+    }
+
+    @Override
+    public void play() {
+        System.out.println(name + " is playing");
+    }
+}
+
+public class advanced1 {
+
+    public static void main(String[] args) {
+
+        Dog dog = new Dog("Dog name: Holio");
+        Parrot parrot = new Parrot("Parrot name: Diver");
+
+        dog.showName();
+        dog.eat();
+        dog.sleep();
+        dog.play();
+
+        System.out.println();
+
+        parrot.showName();
+        parrot.eat();
+        parrot.sleep();
+        parrot.play();
+    }
+}
+    `,
+  },
+
+  {
+    id: 6,
+    title: "inhertiance",
+    description: "simple inheritance",
+    code: `
+      package inheritance;
+
+class Animal{
+    String animalName;
+
+    Animal(String animalName){
+        this.animalName = animalName;
+    }
+    void animalName(){
+        System.out.println(animalName); 
+    }
+}
+
+class Bird extends Animal{
+    String birdName;
+
+    Bird(String animalName, String birdName){
+        super(animalName);
+        this.birdName = birdName;
+    }
+    void animalName(){
+        super.animalName();
+        System.out.println(birdName); 
+    }
+}
+
+public class start1{
+    public static void main(String[] args){
+        Bird bird1 = new Bird("Bird", "Eagle");
+
+        bird1.animalName();
+    }
+}
+    `,
+  },
+  {
+    id: 7,
+    title: "hierchical inheritnace",
+    description: "simple inheritance",
+    code: `
+      package inheritance;
+
+class Calc1{
+    int a , b, sum;
+    void calcSum(int a, int b){
+        sum = a+b;
+        System.out.println(sum);
+    }
+}
+
+class Calc2 extends Calc1{
+    int a,b,minus;
+    void calcMinus(int a, int b){
+        minus = a-b;
+        System.out.println(minus);
+    }
+}
+
+class Calc3 extends Calc2{
+    int a,b,multi;
+    void calcMulti(int a, int b){
+        multi = a*b;
+        System.out.println(multi);
+    }
+}
+
+
+public class start {
+    public static void main(String[] args) {
+
+        Calc3 calc = new Calc3();
+
+        calc.calcSum(4,5);
+        calc.calcMinus(4,5);
+        calc.calcMulti(4,5);
+
+    }
+}
+
+    `,
+  },
+  {
+    id: 8,
+    title: "Multiple inheritance",
+    description: "Make a new Student object and pass parameters",
+    code: `
+      package inheritance;
+
+class Person{
+    String name;
+    int id;
+    Person(String name, int id){
+        this.name = name;
+        this.id = id;
+    }
+    public void displayInfo(){
+        System.out.println(name +": "+ id);
+    }
+}
+
+class Student extends Person{
+    String course;
+    float cgpa;
+    Student(String name, int id, String course, float cgpa){
+        super(name, id);
+        this.course = course;
+        this.cgpa = cgpa;
+    }
+    @Override
+    public void displayInfo(){
+        System.out.println("Student details: ");
+        super.displayInfo();
+        System.out.println(course + cgpa);
+    }
+}
+
+class Teacher extends Person{
+    String course;
+    double salary;
+    Teacher(String name, int id, String course, double salary){
+        super(name, id);
+        this.course = course;
+        this.salary = salary;
+    }
+    @Override
+    public void displayInfo(){
+        System.out.println("Teacher details: ");
+        super.displayInfo();
+        System.out.println(course + salary);
+    }
+}
+
+
+public class start3{
+    public static void main(String[] args){
+        Teacher teacher = new Teacher("Name", 1234, "CSe", 1235);
+
+        teacher.displayInfo();
+    }
+}
+    `,
+  },
 ];
