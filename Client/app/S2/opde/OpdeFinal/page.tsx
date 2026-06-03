747a733b058426ed9afa424f7773c1073667bb66
RabbitJahir <alltimer785@gmail.com>
2026-06-03 10:21:08 +0600

    mhm

--- Client/app/S2/opde/OpdeFinal/page.tsx
+++ Client/app/S2/opde/OpdeFinal/page.tsx
@@ -13,20 +13,12 @@ export default function Page() {
           Final Questions <sub>-Download</sub>
         </Link>
         <Link
-          href="/S2/Opde/LDE-opde.pdf"
+          href="/S2/Opde/LDE-uc-method.pdf"
           target="_blank"
           rel="noopener noreferrer"
           className="SubButton"
         >
-          Non-Homogeneous LDE <sub>-Download, Thanks Forhad</sub>
-        </Link>
-        <Link
-          href="/S2/Opde/uc-method.pdf"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="SubButton"
-        >
-          using UC Method <sub>-Download</sub>
+          Non-Homogeneous LDE uc method <sub>-Download, Thanks Forhad</sub>
         </Link>
         <Link
           href="/S2/Opde/lpde-arbitrary.pdf"
@@ -37,12 +29,12 @@ export default function Page() {
           LPDE arbitrary constant & functions <sub>-Download, by thomas</sub>
         </Link>
         <Link
-          href="/S2/Opde/lpde-cc.pdf"
+          href="/S2/Opde/lpde-ho.pdf"
           target="_blank"
           rel="noopener noreferrer"
           className="SubButton"
         >
-          LPDE constant coefficients <sub>-Download, by thomas</sub>
+          LPDE higher order <sub>-Download, by thomas</sub>
         </Link>
         <Link
           href="/S2/Opde/lpde-lagrange.pdf"
