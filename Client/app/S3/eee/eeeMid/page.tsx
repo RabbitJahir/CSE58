554db3ff5e1acb4a25fbaf53624bfa24a953a366
RabbitJahir <alltimer785@gmail.com>
2026-09-07 17:58:42 +0600

    edc mid diode rectifer

--- Client/app/S3/eee/eeeMid/page.tsx
+++ Client/app/S3/eee/eeeMid/page.tsx
@@ -3,7 +3,18 @@ import Link from "next/link";
 export default function Page() {
   return (
     <div>
-      <main className="mid-container"></main>
+      <main className="mid-container">
+
+        <Link
+            href="/S3/edc/EDC_diode-rectifier-3types.pdf"
+            className="SubButton"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Diode Rectifier 3 types
+        </Link>
+
+      </main>
     </div>
   );
 }
